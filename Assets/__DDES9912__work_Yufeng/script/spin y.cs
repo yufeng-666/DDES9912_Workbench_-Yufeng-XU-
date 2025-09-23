c1097636495b27b7ddd9ae32584aaee84bf41c99
yufeng-666 <2391737049@qq.com>
2025-09-24 05:40:31 +1000

    weather vane

---
+++ Assets/__DDES9912__work_Yufeng/script/spin y.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class spiny : MonoBehaviour
+{
+    public float ySpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,ySpeed,0);
+    }
+}
--- Assets/__DDES9912__work_Yufeng/script/stare.cs
+++ Assets/__DDES9912__work_Yufeng/script/stare.cs
@@ -12,6 +12,23 @@ public class stare : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        transform.LookAt(target); using UnityEngine;
+
+public class CarouselController : MonoBehaviour
+    {
+        public float moveSpeed = 2f;       // 移动速度
+        public Vector3 moveDirection;      // 移动方向
+        public float rotateSpeed = 30f;    // 旋转速度（度/秒）
+
+        void Update()
+        {
+            // 平移移动
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+
+            // 旋转自己
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.Self);
+        }
     }
+
+}
 }
