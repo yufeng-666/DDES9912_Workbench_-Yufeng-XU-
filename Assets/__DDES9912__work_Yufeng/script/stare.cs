b45ee135d296c3cc4707343ecfdac9450316837d
yufeng-666 <2391737049@qq.com>
2025-09-24 04:00:04 +1000

    practice stare and spin

---
+++ Assets/__DDES9912__work_Yufeng/script/stare.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class stare : MonoBehaviour
+{
+    public Transform target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
