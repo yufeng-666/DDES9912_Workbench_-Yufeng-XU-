a1170ce523eb8e3aeb4dc4a48b88d5cd70be5d4e
yufeng-666 <2391737049@qq.com>
2025-10-15 03:45:56 +1100

    change

--- Assets/__DDES9912__work_Yufeng/A1. Assessment jukebox/Script/JukeboxController.cs
+++ Assets/__DDES9912__work_Yufeng/A1. Assessment jukebox/Script/JukeboxController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectAController : MonoBehaviour
+public class JukeboxController : MonoBehaviour
 {
      public GameObject objectA;
     public GameObject objectB;
